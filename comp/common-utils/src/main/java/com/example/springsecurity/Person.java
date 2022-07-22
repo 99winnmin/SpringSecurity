a4b85c3341f825191d06081315a44a93f98734fd
seungmin Ryu <ojysep9987@naver.com>
2022-07-22 15:24:08 +0900

    gradle 빌드 초기환경

---
+++ comp/common-utils/src/main/java/com/example/springsecurity/Person.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person {
+
+    private String name;
+
+}
---
+++ server/basic-test/src/main/java/com/example/springsecurity/BasicTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTestApplication {
+    public static void main(String[] args) {
+
+        Person person = Person.builder().name("test").build();
+        System.out.println(person);
+
+//        SpringApplication.run(BasicTestApplication.class, args);
+    }
+}

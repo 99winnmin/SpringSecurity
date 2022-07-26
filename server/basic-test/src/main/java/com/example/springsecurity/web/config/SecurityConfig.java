0aa8624593c1db3e4dcedd23ac5ed48eecc9972f
seungmin Ryu <ojysep9987@naver.com>
2022-07-26 21:35:48 +0900

    필요없는 코드 삭제

--- server/basic-test/src/main/java/com/example/springsecurity/web/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/example/springsecurity/web/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.example.springsecurity.web.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.DefaultLoginPageConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
+import org.springframework.security.web.authentication.ui.DefaultLogoutPageGeneratingFilter;
 
+@Order(1) // FilterChain이 여러개 일 때 순서를 정해줌
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true) // prePost로 권한 체크를 하겠다
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
